be5e6dc2118937393ebf92fdbae86e5392459bf7
sayden <mariocaster@gmail.com>
2020-02-13 21:05:37 +0100

    Add example of a curl output

---
+++ src/push/mod.rs
@@ -0,0 +1,18 @@
+// *   Trying 172.17.0.2:5986...
+// * TCP_NODELAY set
+// * Connected to 172.17.0.2 (172.17.0.2) port 5986 (#0)
+// > GET /_stats HTTP/1.1
+// > Host: 172.17.0.2:5986
+// > User-Agent: curl/7.68.0
+// > Accept: */*
+// >
+// * Mark bundle as not supporting multiuse
+// < HTTP/1.1 200 OK
+// < Cache-Control: must-revalidate
+// < Content-Length: 14382
+// < Content-Type: application/json
+// < Date: Thu, 13 Feb 2020 20:03:11 GMT
+// < Server: CouchDB/2.3.1 (Erlang OTP/19)
+// <
+// { [14382 bytes data]
+// * Connection #0 to host 172.17.0.2 left intact
