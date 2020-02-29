d5089fdc10463257803b1138cc757cefddf25133
sayden <mariocaster@gmail.com>
2020-02-29 21:26:34 +0100

    Remove unused requests

--- src/server/requests.rs
+++ src/server/requests.rs
@@ -24,26 +24,3 @@ impl Display for Query {
                self.channel.clone().unwrap_or_default())
     }
 }
-
-#[derive(Serialize, Deserialize, Clone)]
-pub struct InsertQueryReq {
-    pub(crate) id: Option<String>,
-    pub(crate) channel: Option<String>,
-}
-
-impl Display for InsertQueryReq {
-    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{}", serde_json::to_string(self).unwrap_or(r#"{"error":true}"#.to_string()))
-    }
-}
-
-#[derive(Deserialize, Serialize, Clone, Debug)]
-pub struct GetReq {
-    pub(crate) key: String
-}
-
-impl Display for GetReq {
-    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        write!(f, "key: {}", self.key)
-    }
-}
