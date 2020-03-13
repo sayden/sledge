1b72afcd31b36191a86ef2d7eea2ecb5996900a0
Mario Castro <mariocaster@gmail.com>
2020-03-13 19:32:40 +0100

    Clippy sort

--- src/channels/sort.rs
+++ src/channels/sort.rs
@@ -5,8 +5,8 @@ use itertools::Itertools;
 use serde_json::{Map, Value};
 
 use crate::channels::error::Error;
-use crate::channels::mutators::*;
 use crate::channels::mutators::Mutator;
+use crate::channels::mutators::*;
 
 #[derive(Debug)]
 pub struct Sort {
@@ -14,57 +14,58 @@ pub struct Sort {
     pub descending: bool,
 }
 
-fn maybe_reverse<T: Into<Value> + Ord>(descending: bool, res: impl Iterator<Item=T> + DoubleEndedIterator) -> Vec<Value> {
+fn maybe_reverse<T: Into<Value> + Ord>(
+    descending: bool,
+    res: impl Iterator<Item = T> + DoubleEndedIterator,
+) -> Vec<Value> {
     if descending {
-        res
-            .sorted().rev().map(|x| x.into()).collect::<Vec<Value>>()
+        res.sorted().rev().map(|x| x.into()).collect::<Vec<Value>>()
     } else {
-        res
-            .sorted().map(|x| x.into()).collect::<Vec<Value>>()
+        res.sorted().map(|x| x.into()).collect::<Vec<Value>>()
     }
 }
 
 impl Mutator for Sort {
     fn mutate(&self, v: &mut Map<String, Value>) -> Result<(), Error> {
-        let value = v.get(&self.modifier.field)
-            .ok_or(Error::FieldNotFoundInJSON(self.modifier.field.to_string()))?;
+        let value = v
+            .get(&self.modifier.field)
+            .ok_or_else(|| Error::FieldNotFoundInJSON(self.modifier.field.to_string()))?;
 
         let array = match value {
             Value::Array(ar) => ar,
-            _ => return Error::NotAnArray(self.modifier.field.to_string()).into()
+            _ => return Error::NotAnArray(self.modifier.field.to_string()).into(),
         };
 
-        let first_item = array.get(0)
-            .ok_or(Error::EmptyArray(self.modifier.field.to_string()))?;
+        let first_item = array
+            .get(0)
+            .ok_or_else(|| Error::EmptyArray(self.modifier.field.to_string()))?;
 
         match first_item {
             Value::String(_) => {
-                let res = array.iter()
-                    .filter_map(|x: &Value| match x {
-                        Value::String(s) => Some(s.clone()),
-                        _ => None,
-                    });
-
-                let res = maybe_reverse(self.descending, res);
-                v[self.modifier.field.as_str()] = Value::from(res);
-            },
-            Value::Number(_) => {
-                let res = array.iter()
-                    .filter_map(|x: &Value| match x {
-                        Value::Number(n) => {
-                            let maybe_i64 = n.as_i64();
-                            if maybe_i64.is_none() {
-                                log::error!("error trying to get an i64 value from json");
-                            }
-                            maybe_i64
-                        }
-                        _ => None
-                    });
+                let res = array.iter().filter_map(|x: &Value| match x {
+                    Value::String(s) => Some(s.clone()),
+                    _ => None,
+                });
 
                 let res = maybe_reverse(self.descending, res);
                 v[self.modifier.field.as_str()] = Value::from(res);
             }
-            _ => return Error::SortNotPossibleError.into()
+            Value::Number(_) => {
+                let res = array.iter().filter_map(|x: &Value| match x {
+                    Value::Number(n) => {
+                        let maybe_i64 = n.as_i64();
+                        if maybe_i64.is_none() {
+                            log::error!("error trying to get an i64 value from json");
+                        }
+                        maybe_i64
+                    }
+                    _ => None,
+                });
+
+                let res = maybe_reverse(self.descending, res);
+                v[self.modifier.field.as_str()] = Value::from(res);
+            }
+            _ => return Error::SortNotPossibleError.into(),
         }
 
         Ok(())
@@ -77,6 +78,10 @@ impl Mutator for Sort {
 
 impl fmt::Display for Sort {
     fn fmt(&self, f: &mut Formatter<'_>) -> Result<(), std::fmt::Error> {
-        write!(f, "Sort '{}' field: (descending='{}')", self.modifier.field, self.descending)
+        write!(
+            f,
+            "Sort '{}' field: (descending='{}')",
+            self.modifier.field, self.descending
+        )
     }
 }
